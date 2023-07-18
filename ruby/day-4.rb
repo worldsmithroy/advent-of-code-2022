045849a18332468faf4a829a93b8a63a382fd9c6
Roy Mumaw <roy_mumaw@yahoo.com>
2023-07-18 14:00:50 -0700

    Fix count methods to pass sample-input data

--- ruby/day-4.rb
+++ ruby/day-4.rb
@@ -1,27 +1,27 @@
-# returns whether or not a range completely contains the other
+# Returns whether or not a given set of assignments fully overlap one or the other
 def fully_overlaps? (assignment)
-	range_a = assignment[0]
-	range_b = assignment[1]
-	range_a.include?(range_b) or range_b.include?(range_a)
+	range_a = parse_range(assignment[0])
+	range_b = parse_range(assignment[1])
+	range_a.cover?(range_b) || range_b.cover?(range_a)
 end
 
-def to_range_pair (row_string)
-	row_string.split(",").map{ |raw_range| parse_range }
-end
-
+# Converts a text range into a Ruby Range
 def parse_range (raw_range)
-	endpoints = raw_range.split("-")
-	(endpoints.first..endpoints.last)
+	range = raw_range.split("-")
+	(range.first..range.last)
 end
 
+# Returns the list of assignments where one fully overlaps the other
 def overlaps (data)
 	data.select{ |assignment| fully_overlaps?(assignment) }
 end
 
+# Converts a raw string of input data into a set of pairs
 def parse_data (raw_data)
-	raw_data.split("/n").map{ |assignment| to_range_pair(assignment) }
+	raw_data.split("\n").map{ |assignment| assignment.split(",") }
 end
 
+# Returns a count of all of the assignments that fully overlap one or the other
 def count_full_overlaps(raw_data)
 	data = parse_data(raw_data)
 	overlaps(data).count
@@ -30,3 +30,4 @@ end
 
 puts "Part A:"
 puts count_full_overlaps(ARGV[0])
+puts parse_data(ARGV[0]).count
